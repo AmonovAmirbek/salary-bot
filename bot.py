055e32b7bdbe323d2d5ab61cf144eb8c6d90e19a
Amigo <129387530+AmonovAmirbek@users.noreply.github.com>
2026-04-17 22:02:17 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,8 +133,7 @@ async def handler(msg: types.Message):
         salary = int(hours * RATE)
 
 await msg.answer(f"⏱ {hours:.2f} soat\n💰 {salary:,} so‘m")
-
-        user_state[user_id] = None
+    user_state[user_id] = None
 
 async def main():
     asyncio.create_task(auto_close())
